11d224c6deb5cc83f427baa9e26e11a02e297182
aliakbar24 <ali.akbar@tekschool.us>
2024-07-22 18:57:25 -0500

    How to handel cross browsing

--- src/test/java/crossbrowsing/CrossBrowsingHandling.java
+++ src/test/java/crossbrowsing/CrossBrowsingHandling.java
@@ -1,5 +1,5 @@
 package crossbrowsing;
-
+import java.util.Scanner;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -7,13 +7,18 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class CrossBrowsingHandling {
     public static void main(String[] args){
-        String browserType = "firefox";
+       //Activity is to find a way to ask user where to run
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Which browser would you like to run: ");
+        String browserType = scanner.next();
+
         WebDriver driver;
-        if (browserType.equals("chrome")) {
+
+        if (browserType.equalsIgnoreCase("chrome")) {
             driver = new ChromeDriver();
-        } else if (browserType.equals("firefox")){
+        } else if (browserType.equalsIgnoreCase("firefox")){
             driver = new FirefoxDriver();
-        } else if (browserType.equals("edge")){
+        } else if (browserType.equalsIgnoreCase("edge")){
             driver = new EdgeDriver();
         } else {
             throw new RuntimeException("Wrong Driver");
