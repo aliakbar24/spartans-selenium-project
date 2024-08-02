c4540163f0c4a45e6b59ec3f6c387e656d03bde8
aliakbar24 <ali.akbar@tekschool.us>
2024-08-01 19:49:06 -0500

    Week 2 Lab session

---
+++ src/test/java/synchronization/SynchronizationIntro.java
@@ -0,0 +1,42 @@
+package synchronization;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class SynchronizationIntro {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://retail.tekschool-students.com/");
+        driver.manage().window().maximize();
+        //Applying Implicit Wait
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        //Applying Explicit Wait
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("signinLink")));
+        element.click();
+
+        WebElement inputElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("email")));
+        inputElement.sendKeys("WrongInput@gmail.com");
+
+        WebElement passwordElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("password")));
+        passwordElement.sendKeys("wrongpassword");
+
+        //Simple method chaining
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("loginBtn"))).click();
+
+        WebElement errorElement = wait.until(ExpectedConditions.presenceOfElementLocated(By.className("error")));
+        System.out.println(errorElement.getText());
+
+        driver.quit();
+
+
+    }
+}
